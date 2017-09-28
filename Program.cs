5d2f0016ad0980234a4c3aa59cc10a5cfc95f285
slavihh <32299157+slavihh@users.noreply.github.com>
2017-09-28 20:49:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = int.Parse(Console.ReadLine());
+            while (max != 0)
+            {
+                Console.WriteLine("Heyy mamam");
+            }
+        }
+    }
+}
